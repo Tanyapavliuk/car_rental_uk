c495b38963a3bf79eccdfc330cff601b3260536a
Tanyapavliuk <tanyapavlyk1998@gmail.com>
2023-12-13 22:18:02 +0200

    add custom reusable select

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,88 +1,49 @@
-import { nanoid } from "nanoid";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Formik, Field, Form } from "formik";
-
 import { FilterLabel } from "../ui";
-import makes from "../data/makes.json";
+import { updateFilters } from "../redux/filtersSlice";
+import { CustomSelect } from "./CustmSelect";
 import price from "../data/price.json";
 import label from "../data/filtersLable.json";
-import { useDispatch } from "react-redux";
-import { updateFilters } from "../redux/filtersSlice";
 
 export const Filters = () => {
   const dispatch = useDispatch();
+  const [carBrand, setCarBrand] = useState("");
+  const [rentalPrice, setRentalPrice] = useState("");
   return (
     <Formik
       initialValues={{
-        brand: "",
-        price: "",
         from: "",
         to: "",
       }}
       onSubmit={(values) => {
-        dispatch(updateFilters(values));
+        dispatch(
+          updateFilters({ ...values, brand: carBrand, price: rentalPrice })
+        );
       }}
     >
-      {({ values, errors, touched }) => (
+      {({ errors, touched }) => (
         <Form>
           <div className="flex justify-center items-end mb-[50px] mt-[150px] gap-x-[18px]">
             <div className="flex flex-col">
               <FilterLabel>{label.brand}</FilterLabel>
-              <Field
-                component="select"
-                name="brand"
-                className="py-[14px] px-[18px] outline-none bg-slate-50 rounded-[14px] cursor-pointer"
-              >
-                <option
-                  value=""
-                  key={nanoid()}
-                  className="text-neutral-900 text-lg font-medium manrope leading-tight"
-                >
-                  Enter the text
-                </option>
-                {makes.map((el) => (
-                  <option
-                    key={nanoid()}
-                    value={el.toLocaleLowerCase()}
-                    className="text-neutral-900 text-opacity-20 text-base font-medium manrope leading-tight hover:text-neutral-900 focus:text-neutral-900 active:text-neutral-900 bg-transparent"
-                  >
-                    {el}
-                  </option>
-                ))}
-              </Field>
+              <CustomSelect
+                value={carBrand}
+                setValue={setCarBrand}
+                type="brand"
+              />
             </div>
             <div className="flex flex-col">
               <FilterLabel>{label.hour}</FilterLabel>
-              <div className="relative flex items-center gap-x-1 w-[125px] h-12">
-                <p className="absolute left-4">{price.lable}</p>
-
-                <Field
-                  type="number"
-                  name="price"
-                  component="select"
-                  className=" w-[125px] pl-[43px] py-[14px] bg-slate-50 rounded-[14px] outline-none  cursor-pointer manrope"
-                >
-                  <option
-                    value=""
-                    key={nanoid()}
-                    className="text-neutral-900 text-lg font-medium manrope leading-tight"
-                  ></option>
-                  {price.array.map((el) => (
-                    <option
-                      key={nanoid()}
-                      value={el}
-                      className="text-neutral-900 text-opacity-20 text-base font-medium manrope leading-tight "
-                    >
-                      {el}
-                    </option>
-                  ))}
-                </Field>
-                {values.price === "" && (
-                  <p className="absolute left-[47px] manrope">{label.dolar}</p>
-                )}
-                {values.price !== "" && (
-                  <p className="absolute right-[45px] manrope">{label.dolar}</p>
-                )}
-              </div>
+              <CustomSelect
+                data={price.array}
+                weight="w-[125px]"
+                label={`${[price.lable, label.dolar]}`}
+                value={rentalPrice}
+                setValue={setRentalPrice}
+                type="price"
+              />
             </div>
             <div className="flex flex-col">
               <FilterLabel>{label.mileage}</FilterLabel>
@@ -114,6 +75,15 @@ export const Filters = () => {
               className=" cursor-pointer w-[136px] h-12 px-11 py-3.5 bg-blue-500 rounded-xl justify-center items-center inline-flex text-white text-sm font-semibold manrope leading-tight"
             >
               Search
+            </button>
+            <button
+              type="reset"
+              onClick={() => {
+                setCarBrand(""), setRentalPrice("");
+              }}
+              className=" cursor-pointer w-[136px] h-12 px-11 py-3.5 bg-blue-500 rounded-xl justify-center items-center inline-flex text-white text-sm font-semibold manrope leading-tight"
+            >
+              Reset
             </button>
           </div>
         </Form>
